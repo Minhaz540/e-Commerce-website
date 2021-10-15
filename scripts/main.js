0e3256bee348d0cc9b9e62d23081080ac041e30f
Minhaz <minhazrabbi93041@gmail.com>
2021-10-15 16:38:59 +0600

    searchbar and mixitup added

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
+// navBar fade animation
 $(function () {
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 80) {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 80) {
 			$(".main-nav").addClass("sticky-css");
 		} else {
 			$(".main-nav").removeClass("sticky-css");
 		}
-    });
+	});
+	// searching
+	$("#searching").on("click", function() {
+		$(".search-bar-wrapper-hide").addClass("search-bar-wrapper").removeClass("search-bar-wrapper-hide");
+	});
+	// close button for close searching
+	$(".cls-btn").on("click", function() {
+		$(".search-bar-wrapper").removeClass("search-bar-wrapper").addClass("search-bar-wrapper-hide");
+	})
 });
+
 // mixitup
 var containerEl = document.querySelector(".mix-container");
 var mixer = mixitup(containerEl);
