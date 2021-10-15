ce40b77b5ac5ef4aa2725de35cad7b276d90ebdf
Minhaz <minhazrabbi93041@gmail.com>
2021-10-15 20:34:12 +0600

    favourite offers adding

--- scripts/main.js
+++ scripts/main.js
@@ -8,15 +8,36 @@ $(function () {
 		}
 	});
 	// searching
-	$("#searching").on("click", function() {
-		$(".search-bar-wrapper-hide").addClass("search-bar-wrapper").removeClass("search-bar-wrapper-hide");
+	$("#searching").on("click", function () {
+		$(".search-bar-wrapper-hide")
+			.addClass("search-bar-wrapper")
+			.removeClass("search-bar-wrapper-hide");
 	});
 	// close button for close searching
-	$(".cls-btn").on("click", function() {
-		$(".search-bar-wrapper").removeClass("search-bar-wrapper").addClass("search-bar-wrapper-hide");
-	})
+	$(".cls-btn").on("click", function () {
+		$(".search-bar-wrapper")
+			.removeClass("search-bar-wrapper")
+			.addClass("search-bar-wrapper-hide");
+	});
 });
 
 // mixitup
 var containerEl = document.querySelector(".mix-container");
 var mixer = mixitup(containerEl);
+
+var toTopbtn = document.getElementsByClassName("goTop")[0];
+var pin = document.getElementById("pin");
+var startingPoint = pin.offsetTop;
+function backTotop() {
+	if (window.pageYOffset <= startingPoint) {
+		toTopbtn.classList.add("goTop-hidden");
+		toTopbtn.classList.remove("goTop");
+	} else {
+		toTopbtn.classList.add("goTop");
+		toTopbtn.classList.remove("goTop-hidden");
+	}
+}
+//back to top
+window.onscroll = function () {
+	backTotop();
+};
